3409c376ff5693d18e54d96e46bd46553ff73d24
Arek <areksonic@gmail.com>
2025-02-24 23:21:05 +0100

    Hey Im walkin here

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ let ctx = c.getContext("2d");
 
 // Precautions of pixelart
 
-ctx.imageSmoothingEnabled = false;
-ctx.webkitImageSmoothingEnabled = false;
-ctx.mozImageSmoothingEnabled = false;
-ctx.msImageSmoothingEnabled = false;
+
 
 // Canvas Resizing
 
@@ -16,11 +13,11 @@ function resizeCanvas() {
     const scale = window.devicePixelRatio || 1;
     canvas.width = canvas.clientWidth * scale;
     canvas.height = canvas.clientHeight * scale;
-    ctx.scale(scale, scale);
 
     Cx = c.width / 2;
     Cy = c.height / 2;
 
+    ctx.imageSmoothingEnabled = false;
     console.log("Resizing canvas")
 }
 
@@ -60,18 +57,89 @@ for(let key in assets){
 
 // Rendering
 
-let scale = 5;
+let lastTime = performance.now();
+let fps = 0;
+let frames = 0;
 
 function draw(){
+    frames++;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 
-    ctx.drawImage(images.player, (images.player.width/9)*0, 0, images.player.width/9, images.player.height, Cx-(images.player.width/9*scale/2), Cy-(images.player.height*scale/2), images.player.width/9*scale, images.player.height*scale);
+    ctx.drawImage(images.player, (images.player.width/9)*0, 0, images.player.width/9, images.player.height, player.x, player.y, images.player.width/9*player.scale, images.player.height*player.scale);
 
-    console.log("Rendered frame")
 
+
+
+
+    // FPS counter
+    let now = performance.now();
+    let delta = now - lastTime;
+
+    if(delta>= 1000){
+        fps = frames;
+        frames = 0;
+        lastTime = now;
+        console.log("FPS: " + fps);
+    }
     requestAnimationFrame(draw);
 }
+
+// Input
+
+// - keydrown dependency
+
+kd.W.down(()=>{
+    if(player.y > 0){
+        player.y -= player.speed;
+    }
+})
+
+kd.S.down(()=>{
+    if(player.y < canvas.height - images.player.height*player.scale){
+        player.y += player.speed;
+    }
+})
+
+kd.A.down(()=>{
+    if(player.x > 0){
+        player.x -= player.speed;
+    }
+})
+
+kd.D.down(()=>{
+    if(player.x < canvas.width - images.player.width/9*player.scale){
+        player.x += player.speed;
+    }
+})
+
+
+kd.run(()=>{
+    kd.tick();
+})
+
+
+// Objects
+
+function Player(name, x, y, scale=4, speed=3){
+    this.name = name;
+    this.x = Cx-(images.player.width/9*scale/2);
+    this.y = Cy-(images.player.height*scale/2);
+    this.scale = scale;
+    this.speed = speed;
+
+    this.animate = function(event){
+        switch(event){
+            case "idle":
+
+        }
+    }
+}
+
+
+const player = new Player("Player");
+
+console.log(player)
 
 
 
