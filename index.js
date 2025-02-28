d0dba1f97c9497144a056210958472a14da2afcd
Arek <areksonic@gmail.com>
2025-02-28 22:21:47 +0100

    e

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-let currentAnim = "idle"
+// Canvas Setup
 
-// Canvas initalization
+const c = document.getElementById("canvas");
+const ctx = c.getContext("2d");
 
-let c = document.getElementById("canvas");
-let ctx = c.getContext("2d");
 
-// Canvas Resizing
+// Automatic canvas resizing
 
 function resizeCanvas() {
     const scale = window.devicePixelRatio || 1;
@@ -16,131 +15,191 @@ function resizeCanvas() {
     Cy = c.height / 2;
 
     ctx.imageSmoothingEnabled = false;
-    console.log("Resizing canvas")
+
+    console.log(`Resized canvas: x: ${c.width}, y: ${c.height}`);
 }
+
+
+// Canvas resizing initialization
 
 resizeCanvas();
 
-// Automatic resizing of canvas
-
 window.addEventListener('resize', resizeCanvas, false);
+
 
 // Center of the canvas
 
 Cx = c.width / 2;
 Cy = c.height / 2;
 
+
 // Asset loader
+
+// Path library
 
 let assets = {
     background: "assets/Tileset/spr_tileset_sunnysideworld_16px.png",
-    playerIdle: "assets/Characters/Human/IDLE/base_idle_strip9.png",
-    playerWalk: "assets/Characters/Human/WALKING/base_walk_strip8.png",
-    playerRun: "assets/Characters/Human/RUN/base_run_strip8.png",
-    playerHatBowlhairIdle: "assets/Characters/Human/IDLE/bowlhair_idle_strip9.png",
-    playerHatBowlhairWalk: "assets/Characters/Human/WALKING/bowlhair_walk_strip8.png",
-    playerHatBowlhairRun: "assets/Characters/Human/RUN/bowlhair_run_strip8.png",
+    player: {
+        idle_base: "assets/Characters/Human/IDLE/base_idle_strip9.png",
+
+        walk_base: "assets/Characters/Human/WALKING/base_walk_strip8.png",
+    }
 }
+
+
+// Loading assets
 
 let loadedAssets = 0;
 let images = {};
 let totalAssets = Object.keys(assets).length;
 
 for(let key in assets){
-    images[key] = new Image();
-    images[key].src = assets[key];
-    images[key].onload = () => {
-        loadedAssets ++;
-        if(loadedAssets === totalAssets){
-            console.log(loadedAssets + "/" + totalAssets + " assets loaded");
-            draw();
+    if(typeof assets[key] === "string"){
+        images[key] = new Image();
+        images[key].src = assets[key];
+        images[key].onload = () => {
+            loadedAssets ++;
+
+            if(loadedAssets === totalAssets){
+                console.log(loadedAssets + "/" + totalAssets + " assets loaded", images);
+                draw();
+            }
+        }
+    } else
+    {
+        images[key] = {};
+        let localTotalAssets = Object.keys(assets[key]).length;
+        let localLoadedAssets = 0;
+
+        for(let localKey in assets[key]){
+            images[key][localKey] = new Image();
+            images[key][localKey].src = assets[key][localKey];
+            images[key][localKey].onload = () => {
+                localLoadedAssets ++;
+                if(localLoadedAssets === localTotalAssets){
+                    loadedAssets ++;
+
+                    if(loadedAssets === totalAssets){
+                        console.log(loadedAssets + "/" + totalAssets + " assets loaded", images);
+                        draw();
+                    }
+                }
+            }
+
         }
     }
 }
 
-// Rendering
+
+// FPS counter
 
 let lastTime = performance.now();
+let fpsLastTime = lastTime;
 let fps = 0;
 let frames = 0;
+let deltaTime;
+
+// Main Rendering
 
 function draw(){
+    let now = performance.now();
+    deltaTime = now - lastTime;
+    lastTime = now;
+
     frames++;
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    player.draw();
+    debugWindow();
 
 
 
-    player.draw(currentAnim);
 
 
     // FPS counter
-    let now = performance.now();
-    let delta = now - lastTime;
+    
 
-    if(delta>= 1000){
+    if(now-fpsLastTime >= 1000){
         fps = frames;
         frames = 0;
-        lastTime = now;
+        fpsLastTime = now;
         console.log("FPS: " + fps);
     }
+
     requestAnimationFrame(draw);
 }
 
+
 // Input
 
-// - keydrown dependency
-
 kd.W.down(()=>{
+    
+    player.currentAnim = "walk";
 
-    currentAnim = "walk";
-
-    if(player.y > 0){
+    if(player.y-player.hitbox.y> 0){
         player.y -= player.speed;
     }
 })
 
 kd.W.up(()=>{
-    currentAnim = "idle";
+    
+    player.currentAnim = "idle";
 })
 
 kd.S.down(()=>{
 
-    currentAnim = "walk";
+    player.currentAnim = "walk";
 
-    if(player.y < canvas.height - images.playerIdle.height*player.scale){
+    if(player.y+player.hitbox.y < canvas.height){
         player.y += player.speed;
     }
 })
 
 kd.S.up(()=>{
-    currentAnim = "idle";
+    player.currentAnim = "idle";
+    
 })
 
 kd.A.down(()=>{
-    currentAnim = "walk";
 
-    if(player.x > 0){
+    player.currentAnim = "walk";
+
+    if(player.x-player.hitbox.x> 0){
         player.x -= player.speed;
     }
 })
 
-kd.A.up(()=>{    
-    currentAnim = "idle";
+kd.A.up(()=>{
+    player.currentAnim = "idle";
+    
 })
 
 kd.D.down(()=>{
 
-    currentAnim = "walk";
+    player.currentAnim = "walk";
 
-    if(player.x < canvas.width - images.playerIdle.width/9*player.scale){
+    if(player.x+player.hitbox.x < canvas.width){
         player.x += player.speed;
     }
 })
 
 kd.D.up(()=>{
-    currentAnim = "idle";
+    player.currentAnim = "idle";
+    
 })
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -148,100 +207,80 @@ kd.run(()=>{
     kd.tick();
 })
 
+// #Classes#
 
+// Player
 
-// Objects
-
-function Player(name, x, y, facing="right", scale=4, speed=3, hat){
-    this.name = name;
-    this.x = Cx-(images.playerIdle.width/9*scale/2);
-    this.y = Cy-(images.playerIdle.height*scale/2);
-    this.scale = scale;
-    this.facing = facing;
-    this.speed = speed;
-    this.hat = hat;
-    this.animFrame = 0;
-
-    this.draw = function(event){
-        let interval;
-        let frames;
-        let frameIndex;
-
-        switch(event){
-            case "idle":
-
-                interval = 5;
-                frames = 8; // Count from 0
-
-                if(this.animFrame >= interval*frames){
-                    this.animFrame = 0;
-                } else {
-                    this.animFrame++;   
-                }
-               
-                frameIndex = Math.floor(this.animFrame/interval);
-                
-                
-                ctx.drawImage(images.playerIdle, (images.playerIdle.width/(frames+1))*frameIndex, 0, images.playerIdle.width/(frames+1), images.playerIdle.height, player.x, player.y, images.playerIdle.width/(frames+1)*player.scale, images.playerIdle.height*player.scale);
-                ctx.drawImage(images.playerHatBowlhairIdle, (images.playerHatBowlhairIdle.width/(frames+1))*frameIndex, 0, images.playerHatBowlhairIdle.width/(frames+1), images.playerHatBowlhairIdle.height, player.x, player.y, images.playerHatBowlhairIdle.width/(frames+1)*player.scale, images.playerHatBowlhairIdle.height*player.scale);
-
-                
-                break;
-            case "walk":
-
-                interval = 5;
-                frames = 7; // Count from 0
-
-                if(this.animFrame >= interval*frames){
-                    this.animFrame = 0;
-                } else {
-                    this.animFrame++;   
-                }
-            
-                frameIndex = Math.floor(this.animFrame/interval);
-
-                if(this.facing === "right"){
-                    ctx.drawImage(images.playerWalk, (images.playerWalk.width/(frames+1))*frameIndex, 0, images.playerWalk.width/(frames+1), images.playerWalk.height, player.x, player.y, images.playerWalk.width/(frames+1)*player.scale, images.playerWalk.height*player.scale);
-                    ctx.drawImage(images.playerHatBowlhairWalk, (images.playerHatBowlhairWalk.width/(frames+1))*frameIndex, 0, images.playerHatBowlhairWalk.width/(frames+1), images.playerHatBowlhairWalk.height, player.x, player.y, images.playerHatBowlhairWalk.width/(frames+1)*player.scale, images.playerHatBowlhairWalk.height*player.scale);
-                } else {
-                    ctx.drawImage(images.playerWalk, (images.playerWalk.width/(frames+1))*frameIndex, 0, images.playerWalk.width/(frames+1), images.playerWalk.height, player.x, player.y, images.playerWalk.width/(frames+1)*player.scale, images.playerWalk.height*player.scale);
-                    ctx.drawImage(images.playerHatBowlhairWalk, (images.playerHatBowlhairWalk.width/(frames+1))*frameIndex, 0, images.playerHatBowlhairWalk.width/(frames+1), images.playerHatBowlhairWalk.height, player.x, player.y, images.playerHatBowlhairWalk.width/(frames+1)*player.scale, images.playerHatBowlhairWalk.height*player.scale);
-                }
-            
-
-                break;
-            case "run":
-                interval = 5;
-                frames = 7; // Count from 0
-
-                if(this.animFrame >= interval*frames){
-                    this.animFrame = 0;
-                } else {
-                    this.animFrame++;   
-                }
-            
-                frameIndex = Math.floor(this.animFrame/interval);
-
-                if(this.facing === "right"){
-                    ctx.drawImage(images.playerRun, (images.playerRun.width/(frames+1))*frameIndex, 0, images.playerRun.width/(frames+1), images.playerRun.height, player.x, player.y, images.playerRun.width/(frames+1)*player.scale, images.playerRun.height*player.scale);
-                    ctx.drawImage(images.playerHatBowlhairRun, (images.playerHatBowlhairRun.width/(frames+1))*frameIndex, 0, images.playerHatBowlhairRun.width/(frames+1), images.playerHatBowlhairRun.height, player.x, player.y, images.playerHatBowlhairRun.width/(frames+1)*player.scale, images.playerHatBowlhairRun.height*player.scale);
-                } else {
-                    ctx.drawImage(images.playerRun, (images.playerRun.width/(frames+1))*frameIndex, 0, images.playerRun.width/(frames+1), images.playerRun.height, player.x, player.y, images.playerRun.width/(frames+1)*player.scale, images.playerRun.height*player.scale);
-                    ctx.drawImage(images.playerHatBowlhairRun, (images.playerHatBowlhairRun.width/(frames+1))*frameIndex, 0, images.playerHatBowlhairRun.width/(frames+1), images.playerHatBowlhairRun.height, player.x, player.y, images.playerHatBowlhairRun.width/(frames+1)*player.scale, images.playerHatBowlhairRun.height*player.scale);
-                }
-            
-
-                break;
+class Player {
+    constructor(x, y, scale = 4, speed = 3, currentAnim = "idle", cosmetics, hitbox = {x: 6, y: 10, width: 11, height: 15}) {
+        this.x = x;
+        this.y = y;
+        this.scale = scale;
+        this.speed = speed;
+        this.currentAnim = currentAnim;
+        this.animations = {
+            idle: {
+                totalFrames: 9,
+                interval: 80
+            },
+            walk: {
+                totalFrames: 8,
+                interval: 80
+            }
         }
-    };
+        this.animAccumulator = 0;
+        this.animFrame = 0;
+        this.cosmetics = cosmetics;
+        this.hitbox = {x: hitbox.x * this.scale, y: hitbox.y * this.scale, width: hitbox.width * this.scale, height: hitbox.height * this.scale};
+        this.centerOrigin = {x: this.hitbox.width /2, y: this.hitbox.height /2};
+        this.width = images.player.idle_base.width * this.scale;
+        this.height = images.player.idle_base.height * this.scale;
 
+        this.draw = (currentAnim = this.currentAnim) => {
+        
+            
+            this.animAccumulator += deltaTime;
+            while (this.animAccumulator >= this.animations[this.currentAnim].interval) {
+                this.animAccumulator -= this.animations[this.currentAnim].interval;
+                this.animFrame++;
+                if (this.animFrame >= this.animations[this.currentAnim].totalFrames) {
+                    this.animFrame = 0;
+                }
+            }            
+
+
+            this.currentAnim = currentAnim;
+
+            switch(currentAnim){
+                case "idle":
+                    ctx.drawImage(images.player.idle_base, images.player.idle_base.width / this.animations[this.currentAnim].totalFrames * this.animFrame, 0, images.player.idle_base.width / this.animations[this.currentAnim].totalFrames, images.player.idle_base.height, this.x-(images.player.idle_base.width*this.scale/this.animations[this.currentAnim].totalFrames/2), this.y-(images.player.idle_base.height*this.scale/2), images.player.idle_base.width / this.animations[this.currentAnim].totalFrames * this.scale, images.player.idle_base.height * this.scale);
+                    break;
+                case "walk":
+                    ctx.drawImage(images.player.walk_base, images.player.walk_base.width / this.animations[this.currentAnim].totalFrames * this.animFrame, 0, images.player.walk_base.width / this.animations[this.currentAnim].totalFrames, images.player.walk_base.height, this.x-(images.player.walk_base.width*this.scale/this.animations[this.currentAnim].totalFrames/2), this.y-(images.player.walk_base.height*this.scale/2), images.player.walk_base.width / this.animations[this.currentAnim].totalFrames * this.scale, images.player.walk_base.height * this.scale);
+                    break;
+            }
+        }
+    }
 }
 
-
-const player = new Player("Player");
-
-console.log(player)
+const player = new Player(Cx, Cy);
 
 
+// Debug Window
 
+let debugWindowObject = document.getElementById("debugWindow");
 
+function debugWindow(){
+    debugWindowObject.innerText = `
+    Player:
+        x: ${player.x}
+        y: ${player.y}
+        scale: ${player.scale}
+        speed: ${player.speed}
+        currentAnim: ${player.currentAnim}
+        hitbox: ${player.hitbox.x}, ${player.hitbox.y}, ${player.hitbox.width}, ${player.hitbox.height}
+        centerOrigin: ${player.centerOrigin.x}, ${player.centerOrigin.y}
+        width: ${player.width}
+        height: ${player.height}
+    `
+}
