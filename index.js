87ccd06dec4c2ab084043ca65d9bb270c658452d
Arek <areksonic@gmail.com>
2025-03-01 23:02:02 +0100

    E

--- index.js
+++ index.js
@@ -41,9 +41,11 @@ let assets = {
     background: "assets/Tileset/spr_tileset_sunnysideworld_16px.png",
     player: {
         idle_base: "assets/Characters/Human/IDLE/base_idle_strip9.png",
+        l_idle_base: "assets/Characters/Human/IDLE/l_base_idle_strip9.png",
 
         walk_base: "assets/Characters/Human/WALKING/base_walk_strip8.png",
-    }
+        l_walk_base: "assets/Characters/Human/WALKING/l_base_walk_strip8.png",
+        }
 }
 
 
@@ -99,6 +101,8 @@ let fps = 0;
 let frames = 0;
 let deltaTime;
 
+let isDebugOn = true;
+
 // Main Rendering
 
 function draw(){
@@ -111,7 +115,7 @@ function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     player.draw();
-    debugWindow();
+
 
 
 
@@ -121,6 +125,11 @@ function draw(){
     
 
     if(now-fpsLastTime >= 1000){
+
+        if(isDebugOn){
+            debugWindow();
+        }
+
         fps = frames;
         frames = 0;
         fpsLastTime = now;
@@ -134,36 +143,69 @@ function draw(){
 // Input
 
 kd.W.down(()=>{
-    
-    player.currentAnim = "walk";
+
+    player.up = true;
+
+    if(player.facing == "right"){
+        player.currentAnim = "walk";
+    } else{
+        player.currentAnim = "l_walk";
+    }
 
     if(player.y-player.hitbox.y> 0){
         player.y -= player.speed;
     }
+
 })
 
 kd.W.up(()=>{
     
-    player.currentAnim = "idle";
+    player.up = false;
+
+    if(player.facing == "right"){
+        player.currentAnim = "idle";
+    } else{
+        player.currentAnim = "l_idle";
+    }
+
+    
+
+
 })
 
 kd.S.down(()=>{
 
+    player.down = true;
     player.currentAnim = "walk";
 
-    if(player.y+player.hitbox.y < canvas.height){
+    if(player.facing == "right"){
+        player.currentAnim = "walk";
+    } else{
+        player.currentAnim = "l_walk";
+    }
+
+    if(player.y+player.hitbox.height < canvas.height){
         player.y += player.speed;
     }
 })
 
 kd.S.up(()=>{
-    player.currentAnim = "idle";
+
+    player.down = false;
+
+    if(player.facing == "right"){
+        player.currentAnim = "idle";
+    } else{
+        player.currentAnim = "l_idle";
+    }
     
 })
 
 kd.A.down(()=>{
 
-    player.currentAnim = "walk";
+    player.left = true;
+    player.currentAnim = "l_walk";
+    player.facing = "left";
 
     if(player.x-player.hitbox.x> 0){
         player.x -= player.speed;
@@ -171,26 +213,29 @@ kd.A.down(()=>{
 })
 
 kd.A.up(()=>{
-    player.currentAnim = "idle";
+
+    player.left = false;
+    player.currentAnim = "l_idle";
     
 })
 
 kd.D.down(()=>{
 
+    player.right = true;
     player.currentAnim = "walk";
+    player.facing = "right";
 
-    if(player.x+player.hitbox.x < canvas.width){
+    if(player.x+player.hitbox.width < canvas.width){
         player.x += player.speed;
     }
 })
 
 kd.D.up(()=>{
+
+    player.right = false;
     player.currentAnim = "idle";
     
 })
-
-
-
 
 
 
@@ -205,6 +250,12 @@ kd.D.up(()=>{
 
 kd.run(()=>{
     kd.tick();
+
+    if((player.right || player.left) && (player.up || player.down)){
+        player.speed = 2.121;
+    } else {
+        player.speed = 3;
+    }
 })
 
 // #Classes#
@@ -212,9 +263,14 @@ kd.run(()=>{
 // Player
 
 class Player {
-    constructor(x, y, scale = 4, speed = 3, currentAnim = "idle", cosmetics, hitbox = {x: 6, y: 10, width: 11, height: 15}) {
+    constructor(x, y, scale = 4, speed = 3, currentAnim = "idle", cosmetics, hitbox = {x: 6, y: 10, width: 8, height: 7}) {
         this.x = x;
         this.y = y;
+        this.up = false;
+        this.down = false;
+        this.left = false;
+        this.right = false;
+        this.facing = "right";
         this.scale = scale;
         this.speed = speed;
         this.currentAnim = currentAnim;
@@ -223,17 +279,24 @@ class Player {
                 totalFrames: 9,
                 interval: 80
             },
+            l_idle: {
+                totalFrames: 9,
+                interval: 80
+            },
             walk: {
                 totalFrames: 8,
                 interval: 80
-            }
+            },
+            l_walk: {
+                totalFrames: 8,
+                interval: 80}
         }
         this.animAccumulator = 0;
         this.animFrame = 0;
         this.cosmetics = cosmetics;
         this.hitbox = {x: hitbox.x * this.scale, y: hitbox.y * this.scale, width: hitbox.width * this.scale, height: hitbox.height * this.scale};
         this.centerOrigin = {x: this.hitbox.width /2, y: this.hitbox.height /2};
-        this.width = images.player.idle_base.width * this.scale;
+        this.width = images.player.idle_base.width * this.scale / this.animations[this.currentAnim].totalFrames;
         this.height = images.player.idle_base.height * this.scale;
 
         this.draw = (currentAnim = this.currentAnim) => {
@@ -251,13 +314,41 @@ class Player {
 
             this.currentAnim = currentAnim;
 
+
+
             switch(currentAnim){
                 case "idle":
+
+                    if (this.animFrame >= this.animations[this.currentAnim].totalFrames) {
+                        this.animFrame = 0;
+                    }
+
                     ctx.drawImage(images.player.idle_base, images.player.idle_base.width / this.animations[this.currentAnim].totalFrames * this.animFrame, 0, images.player.idle_base.width / this.animations[this.currentAnim].totalFrames, images.player.idle_base.height, this.x-(images.player.idle_base.width*this.scale/this.animations[this.currentAnim].totalFrames/2), this.y-(images.player.idle_base.height*this.scale/2), images.player.idle_base.width / this.animations[this.currentAnim].totalFrames * this.scale, images.player.idle_base.height * this.scale);
                     break;
+                case "l_idle":
+
+                    if (this.animFrame >= this.animations[this.currentAnim].totalFrames) {
+                        this.animFrame = 0;
+                    }
+
+                    ctx.drawImage(images.player.l_idle_base, images.player.l_idle_base.width / this.animations[this.currentAnim].totalFrames * this.animFrame, 0, images.player.l_idle_base.width / this.animations[this.currentAnim].totalFrames, images.player.l_idle_base.height, this.x-(images.player.l_idle_base.width*this.scale/this.animations[this.currentAnim].totalFrames/2), this.y-(images.player.l_idle_base.height*this.scale/2), images.player.l_idle_base.width / this.animations[this.currentAnim].totalFrames * this.scale, images.player.l_idle_base.height * this.scale);
+                    break;
                 case "walk":
+
+                    if (this.animFrame >= this.animations[this.currentAnim].totalFrames) {
+                        this.animFrame = 0;
+                    }
+
                     ctx.drawImage(images.player.walk_base, images.player.walk_base.width / this.animations[this.currentAnim].totalFrames * this.animFrame, 0, images.player.walk_base.width / this.animations[this.currentAnim].totalFrames, images.player.walk_base.height, this.x-(images.player.walk_base.width*this.scale/this.animations[this.currentAnim].totalFrames/2), this.y-(images.player.walk_base.height*this.scale/2), images.player.walk_base.width / this.animations[this.currentAnim].totalFrames * this.scale, images.player.walk_base.height * this.scale);
                     break;
+                case "l_walk":
+                    if (this.animFrame >= this.animations[this.currentAnim].totalFrames) {
+                        this.animFrame = 0;
+                    }
+
+                    ctx.drawImage(images.player.l_walk_base, images.player.l_walk_base.width / this.animations[this.currentAnim].totalFrames * this.animFrame, 0, images.player.l_walk_base.width / this.animations[this.currentAnim].totalFrames, images.player.l_walk_base.height, this.x-(images.player.l_walk_base.width*this.scale/this.animations[this.currentAnim].totalFrames/2), this.y-(images.player.l_walk_base.height*this.scale/2), images.player.l_walk_base.width / this.animations[this.currentAnim].totalFrames * this.scale, images.player.l_walk_base.height * this.scale);
+                    break;
+                
             }
         }
     }
@@ -272,9 +363,16 @@ let debugWindowObject = document.getElementById("debugWindow");
 
 function debugWindow(){
     debugWindowObject.innerText = `
+    FPS: ${fps};
+
     Player:
         x: ${player.x}
         y: ${player.y}
+        facing: ${player.facing}
+        up: ${player.up}
+        down: ${player.down}
+        left: ${player.left}
+        right: ${player.right}
         scale: ${player.scale}
         speed: ${player.speed}
         currentAnim: ${player.currentAnim}
